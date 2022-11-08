e63bc72e19e4f9be99903893d82d4fb1b768fde7
hemorgenes <kauan7622@gmail.com>
2022-11-08 16:52:39 -0300

    trabalho

---
+++ src/components/TrackerSection/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+function TrackerSection() {
+  return (
+    <div className={styles.container}>
+      <h2>CONHEÇA O FUTURO ABAIXO</h2>
+    </div>
+  );
+}
+
+export default TrackerSection;
